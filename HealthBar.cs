80ac52d1273fa5a1f96c2c3eb4ff64417a314ea2
WilheimRy <wilheim.ry@gmail.com>
2016-09-21 15:04:59 +1200

    health bar

---
+++ HealthBar.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthBar : MonoBehaviour {
+
+	float healthBarValue;
+
+	float healthBarUpperlimit;
+	bool gameLose;
+
+
+	HealthBar(float healthBarValue)
+	{
+		this.healthBarValue = healthBarValue;
+	}
+
+	// Use this for initialization
+	void Start (float healthBarValue) {
+		if (HealthbarCheck == true) {
+			HealthBar (healthBarValue);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update (float attackPower) {
+		healthBarValue = healthBarValue - attackPower;
+
+		if (healthBarValue < 0.0f) {
+			gameLose = true;
+		}
+	}
+
+	bool HealthbarCheck()
+	{
+		if (healthBarValue > healthBarUpperlimit) {
+			return false;
+		}
+	}
+
+
+
+}
