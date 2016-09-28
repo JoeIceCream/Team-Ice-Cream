4e08433f13011c90483a5369426794855a54aed9
AlphaCheese <joshlewis26@gmail.com>
2016-09-28 21:11:20 +1300

    Add files via upload

--- TowerLocation.cs
+++ TowerLocation.cs
@@ -7,25 +7,31 @@ public class TowerLocation : MonoBehaviour {
         Debug.Log("Tower Location Clicked.");
         TowerManager twrMngr = GameObject.FindObjectOfType<TowerManager>();
 
-        if(twrMngr.selectedTower != null) {
+        if (twrMngr.selectedTower != null) {
             CurrencySystem cs = GameObject.FindObjectOfType<CurrencySystem>();
             int cost = 100; //Fix this
-            if(!cs.BuyTower(cost)) {
+            if (!cs.BuyTower(cost)) {
                 Debug.Log("Not enough monies");
                 return;
             }
             Debug.Log("Deselecting all Towers.");
             SelectTower[] selections = GameObject.FindObjectsOfType<SelectTower>();
 
-            for(int i = 0; i < selections.Length; i++) {
-                if(selections[i].selected != null)
+            for (int i = 0; i < selections.Length; i++) {
+                if (selections[i].selected != null)
                     selections[i].selected.enabled = false;
             }
-
-            //FIXME: right now we assume we are an object nested in a parent.
-            Instantiate(twrMngr.selectedTower, transform.parent.position, transform.parent.rotation);
-            Destroy(transform.parent.gameObject);
-            twrMngr.selectedTower = null;
+            
+            if (transform.parent != null) {
+                Instantiate(twrMngr.selectedTower, transform.parent.position, transform.parent.rotation);
+                Destroy(transform.parent.gameObject);
+                twrMngr.selectedTower = null;
+            }
+            else if(transform != null) {
+                Instantiate(twrMngr.selectedTower, transform.position, transform.rotation);
+                Destroy(transform.gameObject);
+                twrMngr.selectedTower = null;
+            }
         }
         else {
             Debug.Log("No Tower Selected.");
